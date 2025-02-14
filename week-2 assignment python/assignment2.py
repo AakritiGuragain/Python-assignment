a3b65cea65f8c3958d86b09e3fcf681ca2baf707
AakritiGuragain <aakriti.guragain04@gmail.com>
2025-02-14 20:26:57 +0545

    Add files via upload

---
+++ week-2 assignment python/assignment2.py
@@ -0,0 +1,17 @@
+#qno.1
+numbers = input("Enter the numbers seperating by space: ").split()
+#taking input of list
+numbers=[int(num)for num in numbers]
+for num in numbers:
+    if num>50:
+        print("Number more than 50")
+        break
+    if num %5 == 0:
+        continue
+    print(num)
+
+#qno.2
+password= input("Enter your password: ")
+if password>6:
+    print("Weak")
+    
